a51582c1e6b64be758299df7f807b41947dbb265
Davi Alves <alves.pindoba@gmail.com>
2018-08-05 12:33:15 -0300

    modificado

--- objetos.js
+++ objetos.js
@@ -1,13 +1,13 @@
 const produto = {}
 produto.nome = "Celular Ultra Mega"
 produto.preco = 3590.99
-produto['Desconto Legal'] = 0.20
+produto["Desconto Legal"] = 0.2
 
 console.log(produto)
 
 const produto2 = {
-  nome: 'Camisa polo',
-  preco: 79.90,
+  nome: "Camisa polo",
+  preco: 79.9,
   desc: 0.15
 }
 
